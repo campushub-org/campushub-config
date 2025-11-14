053ceabf263a8e8b4a0bdcd951b62fe3ab67c571
NaomiPasc <mbandapambi@icloud.com>
2025-11-14 18:54:03 +0100

    Initial commit

---
+++ src/test/java/campushub/com/campushub_config_service/CampushubConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package campushub.com.campushub_config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampushubConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
